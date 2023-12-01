cf3ca11df6c92a4ad66424b842ce14724f9931f1
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-01 16:46:33 +0700

    fix(bug) : bug isi nomor pospay

--- static/style.js
+++ static/style.js
@@ -1,13 +1,11 @@
-// Inputkan untuk No Handphone/WhatsApp
-const noPospay = document.getElementById("no_pospay");
-// Menambahkan event listener untuk memvalidasi input
-noPospay.addEventListener("input", function() {
-  // Menghapus karakter selain angka dari input
-  const sanitizedInput = noPospay.value.replace(/[^0-9]/g, '');
-  // Membatasi panjang input menjadi maksimal 13 angka
-  if (sanitizedInput.length > 13) {
-    noPospay.value = sanitizedInput.slice(0, 13);
-  } else {
-    noPospay.value = sanitizedInput;
-  }
-});
+document.addEventListener("DOMContentLoaded", function() {
+    const noPospay = document.getElementById("no_pospay");
+    noPospay.addEventListener("input", function() {
+      const sanitizedInput = noPospay.value.replace(/[^0-9]/g, '');
+      if (sanitizedInput.length > 13) {
+        noPospay.value = sanitizedInput.slice(0, 13);
+      } else {
+        noPospay.value = sanitizedInput;
+      }
+    });
+  });
